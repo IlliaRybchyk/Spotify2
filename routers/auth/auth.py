5aca1cfa58495f1e7bf79695cafc83b69c2262a0
IlliaRybchyk <illiarybchyk8@gmail.com>
2025-05-27 21:27:07 +0200

    confid register

--- routers/auth/auth.py
+++ routers/auth/auth.py
@@ -7,28 +7,28 @@ def auth():
 
 
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-# #     if request.method == 'POST':
-# #         username = request.form.get('username')
-# #         password = request.form.get('password')
-# # #        user_id = UserInfo.get_user(username, password)
-# #         # if user_id:
-# #         #     session['username'] = username
-# #         #     session["user_id"] = user_id
-# #         #     return redirect('/dashboard')
-# #         else:
-# #             return 'Неверный логин или пароль'
-#     return render_template('login.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+ #     if request.method == 'POST':
+ #         username = request.form.get('username')
+ #         password = request.form.get('password')
+ # #        user_id = UserInfo.get_user(username, password)
+ #         # if user_id:
+ #         #     session['username'] = username
+ #         #     session["user_id"] = user_id
+ #         #     return redirect('/dashboard')
+ #         else:
+ #             return 'Неверный логин или пароль'
+     return render_template('login.html')
 
-# @app.route("/register", methods=['GET', 'POST'])
-# def register():
-#     if request.method == 'POST':
-#         username = request.form.get('username')
-#         password = request.form.get('password')
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    #   if request.method == 'POST':
+        #   username = request.form.get('username')
+ #         password = request.form.get('password')
 
 # #        UserInfo.create_account(username, password)
 
 #         return f"<h2>Пользователь {username} зарегистрирован!</h2><a href='/login'>Перейти ко входу</a>"
 
-#     return render_template('register.html')
+    return render_template('register.html')
